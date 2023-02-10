b641648124637ceb3cad16a9d4e428c162d606d7
huutai1010 <huutaia21010@gmail.com>
2023-02-10 14:36:37 +0700

    [TaiMH] create home_screen ui, update ios scheme

--- lib/login_creen.dart
+++ lib/login_creen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,47 +18,132 @@ class _LoginPageState extends State<LoginPage> {
         child: Stack(
           alignment: AlignmentDirectional.center,
           children: <Widget>[
+            MyApp(),
             Column(
+              mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Spacer(),
-                Text('Halo Smoothie'),
-                Spacer(),
-                Container(
-                  height: 50,
-                  width: screenWidth * .8,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[200],
-                        shape: const BeveledRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5)))),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Spacer(),
-                        const Text('Login With Google Account'),
-                        Spacer()
-                      ],
-                    ),
-                  ),
-                ),
+                LoginButton(title: 'Login With Google Account'),
+                SizedBox(height: 15),
+                LoginButton(title: 'Login With Facebook Account'),
                 TextButton(
                     onPressed: () {}, child: const Text('Continue as a Guest'))
               ],
             ),
-            IntroComponent(
-              introImageUrl: '',
-              introMessage: '',
-            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final String title;
+
+  LoginButton({required this.title, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: MediaQuery.of(context).size.width * .8,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue[200],
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[Spacer(), Text(title), Spacer()],
+        ),
+      ),
+    );
+  }
+}
+
+class IntroContent extends StatelessWidget {
+  const IntroContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          IntroComponent(
+            introImageUrl: '',
+            introMessage: '',
+          ),
+          Text('Welcome to Halo Smoothie'),
+          Text(
+              "Discover a variety of juice recipes suitable for everyone's health and diversify your menu")
+        ],
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late int selectedPage;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    selectedPage = 0;
+    _pageController = PageController(initialPage: selectedPage);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final pageCount = 3;
+
+    return SafeArea(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Expanded(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (page) {
+              setState(() {
+                selectedPage = page;
+              });
+            },
+            children: List.generate(3, (index) {
+              return IntroContent();
+            }),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: PageViewDotIndicator(
+            currentItem: selectedPage,
+            count: pageCount,
+            unselectedColor: Colors.black26,
+            selectedColor: Colors.blue,
+            duration: Duration(milliseconds: 200),
+            boxShape: BoxShape.rectangle,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Spacer()
+      ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,282 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           //primarySwatch: Colors.blue,
           ),
-      home: LoginPage(),
+      home: HomePageV2(),
     );
   }
 }
+
+class HomePageV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 15.0),
+              child: Row(children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Good morning',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.grey),
+                    ),
+                    Text(
+                      'Mai Huu Tai',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Image.network(
+                  'https://cdn-icons-png.flaticon.com/128/9561/9561514.png',
+                  width: 70,
+                  height: 70,
+                )
+              ]),
+            ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    'Popular product',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 15),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          width: 250,
+                          height: 125,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://media.istockphoto.com/id/821583034/photo/various-fruits-juices.jpg?s=612x612&w=0&k=20&c=oHI_Qv-Ci2vRjiJFYcFY40F-nPGJCRvw6fTHhM-TyUg='),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text('Orang juice mix',
+                            style: TextStyle(fontWeight: FontWeight.w500))
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          width: 250,
+                          height: 125,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://media.istockphoto.com/id/947356112/photo/drinks.jpg?s=612x612&w=0&k=20&c=F6rwi6wrmNzR60EsWu5Tj_MJDujIUNo8y3jGhKT08xI='),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text('Orang juice mix',
+                            style: TextStyle(fontWeight: FontWeight.w500))
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          width: 250,
+                          height: 125,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  'https://wallpapercave.com/wp/wp1907098.jpg'),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text('Orang juice mix',
+                            style: TextStyle(fontWeight: FontWeight.w500))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 15),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Morning product',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.only(right: 15),
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(.4),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text('More'),
+                    ),
+                  )
+                  //ElevatedButton(onPressed: () {}, child: )
+                ],
+              ),
+            ),
+            MenuFruit(),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 15),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Afternoon product',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      margin: EdgeInsets.only(right: 15),
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(.4),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text('More'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            MenuFruit()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuFruit extends StatelessWidget {
+  const MenuFruit({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 15),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: 25),
+                width: 125,
+                height: 150,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://cdn.healthyrecipes101.com/recipes/images/juices/how-long-does-carrot-mango-juice-last-clausqv7y00kzdf1bcipi4s9m.webp?w=1080&q=75')),
+                    color: Colors.grey.withOpacity(.5),
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Orange mix',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              Text('25.000'),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: 25),
+                width: 125,
+                height: 150,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs9rna1yGj7MajCk2m1OR9KTorz6nbJ7kdyw&usqp=CAU')),
+                    color: Colors.grey.withOpacity(.5),
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Orange mix',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              Text('30.000')
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: 25),
+                width: 125,
+                height: 150,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYJKluDnpT86NYNJPr7ESWsr2JMZM_N1sxnJ2KwFpWfpYADduWvlOV9M7LGk9Nvbh4JOg&usqp=CAU')),
+                    color: Colors.grey.withOpacity(.5),
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Orange mix',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              Text('25.000')
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+
+//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYJKluDnpT86NYNJPr7ESWsr2JMZM_N1sxnJ2KwFpWfpYADduWvlOV9M7LGk9Nvbh4JOg&usqp=CAU
+//https://cdn.healthyrecipes101.com/recipes/images/juices/how-long-does-carrot-mango-juice-last-clausqv7y00kzdf1bcipi4s9m.webp?w=1080&q=75

36fe0d4b1118cded75770c29bd88bed22e488cf1
Cuong Dang <cuong.developer19@gmail.com>
2023-02-14 17:23:45 +0700

    Test

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:halo_smoothie/login_creen.dart';
 import 'package:halo_smoothie/main.dart';
+import 'package:http/io_client.dart';
 
 class AuthService {
   handleAuthState() {
@@ -35,6 +38,18 @@ class AuthService {
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
+
+    var client = new IOClient(
+        new HttpClient()..badCertificateCallback = (cert, host, port) => true);
+    var response = await client.post(
+      Uri.https(
+        "10.0.2.2:7237",
+        "/api/auth/customers",
+      ),
+      headers: {"Authorization": "Bearer ${googleAuth?.idToken}"},
+    );
+
+    print(response.body);
 
     print(FirebaseAuth.instance.currentUser!.displayName);
     print(FirebaseAuth.instance.currentUser!.email);
